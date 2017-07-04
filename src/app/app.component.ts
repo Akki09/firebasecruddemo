29ebfa4714b2ba959c449d3427a00cfbecb76a6f
jinalshah999 <jinalshah999@gmail.com>
2017-07-04 17:47:12 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database";
+import 'firebase/storage'
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  arr: FirebaseListObservable<any[]>;
+  public uploadTask: firebase.storage.UploadTask;
+  constructor(public _database: AngularFireDatabase) {}
+  ngOnInit() {
+     this.arr = this._database.list('/products');
+  }
+  add(field,img,price) {
+   this.arr.push({ pname: field.value, createdAt: new Date().toString(),price:price.value, likes: 0,image:img.src});
+   
+  }
+  remove($key) {
+    this.arr.remove($key);
+  }
+  like($key, like) {
+    this.arr.update($key, { likes: like + 1 });
+  }
+  upload(f, img) {
+    
+    let storageReference = firebase.storage().ref('/images/' + f.files[0].name);
+    this.uploadTask = storageReference.put(f.files[0]);
+    this.uploadTask.on('state_changed', function (snapshot) {
+
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log('Upload is paused');
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+      }
+    }, function (error) {
+      // Handle unsuccessful uploads
+    }, function () {
+      
+      // Handle successful uploads on complete
+      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+      storageReference.getDownloadURL().then(function (url) {
+        // Insert url into an <img> tag to "download"
+        img.src = url;
+        
+      });
+ 
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 import { AppComponent } from './app.component';
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabase } from "angularfire2/database";
+
+export const firebaseconfig=
+{
+   apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseconfig)
   ],
-  providers: [],
+  providers: [AngularFireDatabase],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
